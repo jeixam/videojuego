9f5cdbe7b7d24a0d74b27e102816c48956357333
Jeixam <jaime_rodriguez_apps1t1617@gmail.com>
2017-12-15 15:29:13 -0500

    controladors y modelos bien

--- fuel/app/classes/model/listas.php
+++ fuel/app/classes/model/listas.php
@@ -10,7 +10,19 @@ class Model_Listas extends Orm\Model
         (
             'data_type' => 'varchar'   
         ),
+        'id_usuario' => array
+        (
+            'data_type' => 'int'   
+        )
         );
-
+protected static $_belongs_to = array(
+    'usuarios' => array(
+        'key_from' => 'usuario_id',
+        'model_to' => 'Model_users',
+        'key_to' => 'id',
+        'cascade_save' => true,
+        'cascade_delete' => false,
+    )
+);
     
 }
